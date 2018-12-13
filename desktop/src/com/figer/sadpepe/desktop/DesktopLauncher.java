073bf44b204cbdb0cb26e87089c83994d7ba7172
FigerOne <figer1998@freemail.hu>
2018-12-13 21:11:05 +0100

    first commit

---
+++ desktop/src/com/figer/sadpepe/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.figer.sadpepe.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.figer.sadpepe.GameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameMain(), config);
+	}
+}
